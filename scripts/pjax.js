d4cf15f670312392bcdc6131c7629085c36bbc9e
David Stummer <stummer@gmail.com>
2015-02-12 10:14:05 +0000

    commit

--- scripts/pjax.js
+++ scripts/pjax.js
@@ -1,86 +1,58 @@
 (function () {
-    var regexp = /(?:\{\{)([a-zA-Z][^\s\}]+)(?:\}\})/g;
 
-    function render(template, data) {
-        return template.replace(regexp, function (fullMatch, capture) {
-            if (data[capture]) {
-                return data[capture];
-            } else {
-                return fullMatch;
-            }
-        });
-    }
-
-    window.renderTemplate = render;
-})();
-
-/*var tpl = document.getElementById('tpl').innerHTML;
- var tpl2 = renderTemplate(tpl, {
- img        : 'img.png',
- title      : 'My Book',
- description: 'My Book Description'
- });*/
-
-(function () {
-    var routes = [];
-
-    function addRoute(route, callback, scope) {
-        var routeObj = {
-            route   : route,
-            callback: callback,
-            scope   : scope
-        };
-        routes.push(routeObj);
-    }
-
-    function handleRoute(path, noHistory) {
-        var len = routes.length,
-            routeObj,
-            scope;
-
-        for (var i = 0; i < len; i += 1) {
-            routeObj = routes[i];
-            if (path.match(routeObj.route)) {
-                if (routeObj.scope) {
-                    scope = routeObj.scope;
-                } else {
-                    scope = window;
+    // 1: router implementation with functions for adding new routes and handling them
+    var router = {
+        routes: [],
+        add   : function (route, callback, scope) {
+            this.routes.push({
+                route   : route,
+                callback: callback,
+                scope   : scope
+            });
+        },
+        handle: function (path, doPush) {
+            var len = this.routes.length,
+                routeObj,
+                scope,
+                i;
+            for (i = 0; i < len; i += 1) {
+                routeObj = this.routes[i];
+                if (path.match(routeObj.route)) {
+                    if (routeObj.scope) {
+                        scope = routeObj.scope;
+                    } else {
+                        scope = window;
+                    }
+                    if (doPush) {
+                        history.pushState({}, null, path);
+                    }
+                    routeObj.callback.apply(scope, [path]);
+                    return true;
                 }
-                // if from popstate, shouldn't push again
-                if (!noHistory) {
-                    history.pushState({}, null, path);
-                }
-                routeObj.callback.apply(scope, [path]);
-                return true;
             }
+            return false;
         }
-        return false;
-    }
+    };
 
+    // 2: three different ways the url can change, handle all of them
     window.addEventListener('popstate', function () {
-        handleRoute(window.location.href, true);
+        router.handle(window.location.href);
     });
 
-    window.router = {
-        handleRoute: handleRoute,
-        addRoute   : addRoute
+    window.onload = function () {
+        router.handle(window.location.href);
     };
 
     document.addEventListener('click', function (e) {
-        if (e.target.href) {
-            if (router.handleRoute(e.target.href)) {
-                e.preventDefault();
-            }
+        if (e.target.href && router.handle(e.target.href, true)) {
+            e.preventDefault();
         }
-    })
-})();
+    });
 
-(function () {
+    // 3: our client code which hides/shows elements based on the current route
     var pages = {},
-        regexp,// = /([a-z0-9_]+\.html)/;
-        tpl = document.getElementById('tpl').innerHTML;
-
-    regexp = /\?book=([0-9])/;
+        routeRegexp = /\?book=([0-9])/,
+        tpl = Handlebars.compile(document.getElementById('tpl').innerHTML);
 
     function hidePages() {
         var page;
@@ -89,36 +61,26 @@
         }
     }
 
-    function normalizeLink(path) {
-        return path.match(regexp)[1];
-    }
-
     function handlePage(path) {
-        var href = normalizeLink(path),
+        var pageNum = path.match(routeRegexp)[1],
             req;
         hidePages();
-        if (pages[href]) {
-            hidePages();
-            pages[href].style.display = 'inline-block';
+        if (pages[pageNum]) {
+            pages[pageNum].style.display = 'inline-block';
         } else {
             req = new XMLHttpRequest();
-            req.open('get', 'book' + href + '.json', true);
+            req.open('get', 'book' + pageNum + '.json', true);
             req.send();
             req.onload = function () {
                 var data = JSON.parse(this.responseText),
                     div = document.createElement('div');
                 div.id = 'book-content';
-                div.innerHTML = renderTemplate(tpl, data);
+                div.innerHTML = tpl(data);
                 document.querySelector('#app-content').appendChild(div);
-                pages[href] = div;
-                //debugger
+                pages[pageNum] = div;
             };
         }
-    };
+    }
 
-    router.addRoute(regexp, handlePage);
+    router.add(routeRegexp, handlePage);
 })();
-
-onload = function () {
-    router.handleRoute(window.location.href, true);
-}
